1868a1b908fbc9757ada1d5c378c09366b8b35bd
Daniel Richman <main@danielrichman.co.uk>
2008-09-16 17:24:12 +0000

    * Initial commit of first code. Vignere cipher setup and cracking good...    and damn fast too.
    -- Daniel Richman   Tue, 16 Sep 2008 18:24:03 +0100

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/*
+    Cifer: Automating classical cipher cracking in C
+    Copyright (C) 2008  Daniel Richman & Simrun Basuita
+
+    Cifer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cifer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cifer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdinc.h"
+
+int main(int argc, char **argv)
+{
+  cc_setup(argc, argv);
+  crack_vignere(intext, intext_size);
+
+  return 0;
+} 
+
