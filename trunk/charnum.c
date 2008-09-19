8bfcc21bf632e648bee9b79b2a11633390f6ee60
DRebellion1 <DRebellion1@03753b3a-8413-11dd-b2bb-736fb4d8a922>
2008-09-19 15:39:56 +0000

    * Removed charnum.c (replaced by cmdline option - --print-char-table -pct) * Various Makefile improvements   - Object files now have src/stdinc.h as prerequisite   - Added target 'cifer-debug'; builds with debugging symbols   - Added target 'cifer-opt'; builds with full (-O3) optimisations   - Target 'clean' removes both 'cifer-debug' and 'cifer-opt' binaries as well     as 'cifer' and 'dict'
     -- Simrun Basuita  Fri, 19 Sep 2008 16:39:50 +0100

--- trunk/charnum.c
+++
@@ -1,7 +0,0 @@
-#include "src/stdinc.h"
-#define A(x) printf("%c: %i\n", x, CHARNUM(x))
-
-void main()
-{
-  A('t');  A('h');  A('h');  A('e');  A('i');  A('n');  A('e');  A('r');  A('a');  A('n');
-}
