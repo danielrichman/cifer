329b7f887bea732231201e9a4a7d094278019496
DRebellion1 <DRebellion1@03753b3a-8413-11dd-b2bb-736fb4d8a922>
2008-09-19 17:24:41 +0000

    * Added src/action.* to contain functions to control program flow
     -- Simrun Basuita  Fri, 19 Sep 2008 18:24:35 +0100

--- trunk/src/action.h
+++ trunk/src/action.h
@@ -15,35 +15,3 @@
     You should have received a copy of the GNU General Public License
     along with Cifer.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#ifndef STDINC_CIPHERCRACK
-#define STDINC_CIPHERCRACK 1
-
-#include <stdio.h>
-
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
-#include <time.h>
-#include <errno.h>
-#include <sys/file.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
-#include "settings.h"
-#include "macros.h"
-
-#include "dictionary.h"
-#include "interface.h"
-#include "frequency_data.h"
-#include "vigenere.h"
-#include "random_substitution.h"
-#include "ciphers.h"
-#include "utility.h"
-#include "arg.h"
-#include "input.h"
-#include "action.h"
-
-#endif
-
