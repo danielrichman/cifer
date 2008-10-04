dc02e9b922d9a9525da45db907d50c17132f8dce
Simrun Basuita <simrunbasuita@gmail.com>
2008-10-04 19:18:46 +0000

    * Started on rf_bf() * Started editing rf_get_spaces() to set spaces1 and spaces2 * Debug code in main
     -- Simrun Basuita  Sat, 04 Oct 2008 20:18:19 +0100

--- src/rf.c
+++ src/rf.c
@@ -20,21 +20,44 @@
 
 void rf_bf(char *intext, int intext_size, int minrails, int maxrails)
 {
-  int i, j, *spaces_temp;
-
-  /* Setup reusable target space */
-  spaces_temp = malloc( sizeof(int) * maxrails );
-
-  for (i = minrails; i <= maxrails; i++)
+  int i, j, currails; /* Counters */
+  
+  /* Wavelength, for each rails */
+  int wl[maxrails];
+  for (i = 0; i < maxrails; i++)
+    wl[i] =  (i - 1) * 2;
+  
+  /* How many times the wave repeats, and the amount of the wave left over,
+     for each rails */
+  int repeats[maxrails];
+  int repeats_rem[maxrails];
+  for (i = 0; i < maxrails; i++)
   {
-    rf_get_spaces(i, spaces_temp);
-    printf("Rail %2i: %i", i, spaces_temp[0]);
-    for (j = 1; j < i; j++) printf(", %i", spaces_temp[j]);
-    printf("\n");
+    repeats_rem[i] = modp(intext_size, wl[i]);
+    repeats[i] = (intext_size - repeats_rem[i]) / wl[i];
   }
+  
+  /* chcount holds the count of chars for [num_of_rails][line] */
+  /* Padded with -1 */
+  int chcount[maxrails][maxrails]; /* We'll have space left over,
+                                      stop crying and buy yourself some more
+                                      RAM */
+  for (i = 0; i < maxrails; i++) for (j = 0; j < maxrails; j++)
+    chcount[i][j] = -1;
+  
+  /* The first and last line of each wave in rails has 1 ptext char */
+  for (i = 0; i < maxrails; i++)
+  {
+    chcount[i][0] = repeats[i]; /* First */
+    chcount[i][i] = repeats[i]; /* Last */
+  }
+  
+  /* All the others have 2 ptext chars */
+  for (i = 0; i < maxrails; i++) for (j = 1; j < (maxrails - 1); j++)
+    chcount[i][j] = repeats[i] * 2;
 }
 
-void rf_get_spaces(int rail, int *target)
+void rf_get_spaces(int rail, int **target)
 {
   int i, next, middle1, middle2;
 
@@ -50,15 +73,19 @@ void rf_get_spaces(int rail, int *target)
   }
 
   next = rail * 2;
-  printf("rf_get_spaces (%i): middle 1 and 2: %i %i\n", rail, middle1, middle2);
 
   for (i = 0; i < rail; i++)
   {
     if (i < middle1)  next -= 2;
     if (i > middle2)  next += 2;
 
-    target[i] = next;
-    printf("   -> debug, setting %i to new next %i\n", i, next);
+    target[i][0] = next;
+    
+    if ((next - rail) < 0)
+    {
+      target[i][1] = target[i][0];
+    }
+    else if (next == rail) /* First or last */
+      target[i][1] = target[i][0];;
   }
 }
-
