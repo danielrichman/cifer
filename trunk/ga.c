cb41b79bae22e7718362c3ad97b70714516f01ae
DRebellion1 <DRebellion1@03753b3a-8413-11dd-b2bb-736fb4d8a922>
2008-10-20 19:55:04 +0000

    * Added ga.c for testing genetic algorithm
     -- Simrun Basuita  Mon, 20 Oct 2008 20:54:33 +0100

---
+++ trunk/ga.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GA_POPSIZE 2048 // Population size
+#define GA_MAXITER 16348 // Maximum iteration
+#define GA_ELITERATE 0.10 // Elitism rate
+#define GA_MUTRATE 0.25 // Mutation rate
+
+#define TARGET_LEN 20
+int target[TARGET_LEN] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                           0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Target solution
+
+struct ga_memb
+{
+  int sol[TARGET_LEN]; // This member's solution (chromosome)
+  int fitness; // How good the solution is
+};
+
+void init_pop(ga_memb &pop);
+FILE *open_ur(void);
+void zero_fitness(ga_memb &pop);
+void rand_sols(ga_memb &pop);
+void calc_fitness(ga_memb &pop);
+void sort_by_fitness(ga_memb &pop);
+void print_best(ga_memb &pop);
+
+int main(void)
+{
+  struct ga_memb pop_a[GA_POPSIZE], pop_b[GA_POPSIZE];
+  struct ga_memb *pop, *buf;
+  
+  init_pop(pop_a);
+  init_pop(pop_b);
+  
+  pop = &pop_a;
+  buf = &pop_b;
+  
+  for (int i = 0; i < GA_MAXITER; i++);
+  {
+    calc_fitness(*pop); // Fill out ga_memb.fitness
+    sort_by_fitness(*pop);
+    print_best(*pop);
+    
+    if ((*pop)[0].fitness == TARGET_LEN) break; // We can has solution! :)
+    
+    //mate(*pop, *buf) // Mate the population into the buffer
+    //mutate(*buf) // Radiation!
+    
+    //swap_pt(pop, buf) // Swap teh pointerz faw speedz
+  }
+  
+  return 0;
+}
+
+void init_pop(ga_memb &pop)
+{
+  FILE *ur = open_ur();
+  
+  zero_fitness(pop);
+  rand_sols(pop);
+}
+
+FILE *open_ur(void)
+{
+  if ((FILE *fp = fopen("/dev/urandom", "r") == NULL))
+  {
+    printf("\nFailed to fopen() /dev/urandom\n");
+    exit(0);
+  }
+  
+  return fp;
+}
+
+void zero_fitness(ga_memb &pop)
+{
+  for (int i = 0; i < GA_POPSIZE; i++)
+  {
+    (*pop)[i].fitness = 0;
+  }
+}
+
+void rand_sols(ga_memb &pop)
+{
+  for (int i = 0; i < GA_POPSIZE; i++)
+  {
+    for (int j = 0; j < TARGET_LEN; j++)
+    {
+      (*pop)[i].sol[j] = rand();
+    }
+  }
+}
+
+void calc_fitness(ga_memb &pop)
+{
+  unsigned int fitness;
+  
+  for (int i = 0; i < GA_POPSIZE; i++)
+  {
+    fitness = 0;
+    for (int j = 0; j < TARGET_LEN; j++)
+    {
+      if ((*pop)[i].sol[j] == target[j])
+      {
+        fitness += 1;
+      }
+    }
+    (*pop)[i].fitness = fitness;
+  }
+}
+
+// Insertion sort
+void sort_by_fitness(ga_memb &pop)
+{
+  int j;
+  int value;
+  
+  for (int i = 0; i < GA_POPSIZE; i++)
+  {
+    value = (*pop)[i].fitness;
+    j = i - 1;
+    while ((j >= 0) && ((*pop)[i].fitness > value))
+    {
+      (*pop)[j + 1] = (*pop)[j];
+      j -= 1;
+    }
+    (*pop)[j + 1] = value;
+  }
+}
+
+void print_best(ga_memb &pop)
+{
+  printf("Best: %*d (%d%%)\n", TARGET_LEN, (*pop)[0].sol, ((*pop)[0].fitness / TARGET_LEN) * 100);
+}
